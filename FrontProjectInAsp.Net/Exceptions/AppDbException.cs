68a5c214c8ce0349f9d602e775106f49d62cdd8c
unknown <Dunyafm>
2022-03-31 04:17:29 +0400

    Slider creat cruds

---
+++ FrontProjectInAsp.Net/Exceptions/AppDbException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontProjectInAsp.Net.Exceptions
+{
+    public class AppDbException : Exception
+    {
+        public AppDbException(string message) : base(message)
+        {
+        }
+
+    }
+}
